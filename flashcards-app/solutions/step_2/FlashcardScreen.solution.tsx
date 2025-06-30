244b67591d438d154489dffbe229ddd93299b869
Ehren Fox <ehrenfox@gmail.com>
2025-06-30 17:31:51 -0500

    Step 2 solution for adding a card form and logging to console.

---
+++ flashcards-app/solutions/step_2/FlashcardScreen.solution.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Button, Text, FlatList } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../App';
+import Flashcard from '../components/Flashcard';
+import sampleCards from '../data/sampleCards';
+
+const FlashcardScreen: React.FC<
+  NativeStackScreenProps<RootStackParamList, 'Flashcards'>
+> = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, paddingTop: 50 }}>
+      <Button title="Add Card" onPress={() => navigation.navigate('AddCard' as any)} />
+      
+      <FlatList 
+        data={sampleCards}
+        renderItem={({ item }) => <Flashcard question={item.question} answer={item.answer} />}
+      />
+    </View>
+  );
+};
+
+export default FlashcardScreen;
