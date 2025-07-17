cd53e8c97c570f0344f6b9bb09a637787add5ee6
Ehren Fox <ehrenfox@gmail.com>
2025-07-17 17:27:18 -0500

    goat game

--- flashcards-app/data/sampleCards.ts
+++ flashcards-app/data/sampleCards.ts
@@ -1,6 +1,7 @@
 import { FlashcardType } from '../App';
 
 const sampleCards: FlashcardType[] = [
+  { question: 'What is the greatest video game of all time?', answer: 'Super Mario Bros.' },
   { question: 'What is the capital of France?', answer: 'Paris' },
   { question: 'What is 2 + 2?', answer: '4' },
   { question: 'What is the color of the sky?', answer: 'Blue' },
