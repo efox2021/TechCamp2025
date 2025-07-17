30150c96585eb987f53c5f7ab7e0f1722b3a048c
Ehren Fox <ehrenfox@gmail.com>
2025-07-17 08:22:16 -0500

    Update

--- flashcards-app/solutions/step_1/Flashcard.solution.tsx
+++ flashcards-app/solutions/step_1/Flashcard.solution.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 interface FlashcardProps {
   question: string;
